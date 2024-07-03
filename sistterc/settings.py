5a940070c8b1a8bdd35608ba5310e598ba9b0549
Igor Gabriel Monteiro <98916549+IgorG-Monteiro@users.noreply.github.com>
2024-07-03 13:31:09 -0300

    Update settings.py
    retirada de informações

--- sistterc/settings.py
+++ sistterc/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5!mcj_hqju-!q0&9pn@#z(j7i5u(i_2i))bn*n52t5mohz=%et'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
